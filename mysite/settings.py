ad9af0bf3cb1d2a6118164ea62486335653f2bfa
John Link <john.link33@gmail.com>
2022-09-24 11:59:32 -0400

    added requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "studybuddy.apps.StudybuddyConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
