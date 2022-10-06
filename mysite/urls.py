0e104e946561d1452e189d1878c8ed0d2bedc3ee
Vinya Ponugoti <vinyaponugoti@Vinyas-MBP.home>
2022-10-06 13:02:58 -0400

    Created login with google authentication

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('studybuddy/', include('studybuddy.urls')),
+    path('', include('studybuddy.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('logout/', include('studybuddy.urls'))
+    
 ]
--- studybuddy/views.py
+++ studybuddy/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import logout as log_out
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Study Buddy")
+# def index(request):
+#     return HttpResponse("Study Buddy")
+
+def login(request):
+    return render(request,'studybuddy/login.html',{})
+
+def logout(request):
+    log_out(request)
+    return redirect('/')
