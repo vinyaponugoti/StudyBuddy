feec5d7d29a4ba5f33855103ccc307d70367992f
Jim Liang <89550877+uvajim@users.noreply.github.com>
2022-10-09 13:33:37 -0400

    added a login test

--- studybuddy/tests.py
+++ studybuddy/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class LoginTestCase(TestCase):
+
+
+    def login_test(self):
+        user = User.objects.create(username='test_acc')
+        user.set_password('12345')
+        user.is_active = True
+        user.save()
+
+        self.client.login(username='test_acc', password='12345')
+
+        response = self.flow()
+
+        self.assertEqual(200, response.status_code, response.content)
+        self.assertContains(response, 'This profile is already connected to another user account')
+
