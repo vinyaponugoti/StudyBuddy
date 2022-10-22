400c920cf88617b6a4d4524ea20a369ac1addaaa
Jim Liang <89550877+uvajim@users.noreply.github.com>
2022-10-22 16:50:14 -0400

    added models for the search function

--- studybuddy/models.py
+++ studybuddy/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Class(models.Model):
+    course_mnemonic = models.CharField(max_length=5)
+    course_number = models.CharField(max_length=5)
+    other_info = models.CharField(max_length=300)
+
+class Mnemonics(models.Model):
+    course_mnemonics = models.CharField(max_length=5)
+
+
