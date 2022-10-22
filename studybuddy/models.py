c35e2e2fb7648aded44d6a5d0237af9d7236be84
John Link <john.link33@gmail.com>
2022-10-22 16:52:24 -0400

    added model fields

--- studybuddy/models.py
+++ studybuddy/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class SISClass(models.Model):
+    deptDict = {"CS" : "Computer Science"}
+    
+    DeptNnemonic = models.TextField()
+    DeptName = models.TextField()
+    CourseNumber = models.IntegerField()
+    SectionNumber = models.IntegerField()
+    ClassName = models.TextField()
+    SectionName = models.TextField()
+    ProfessorName = models.TextField()
+    AvailableSeats = models.IntegerField()
+    DaysOfTheWeek = models.TextField()
+    Semester = models.TextField()
+    
+    def __str__(self):
+        return self.title
