893e835378ac919132452ceaed5bebcc625654a4
Jim Liang <89550877+uvajim@users.noreply.github.com>
2022-10-24 14:10:47 -0400

    URL FIX

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('logout/', include('studybuddy.urls')),
-    path('login/', include('studybuddy.urls'))
+    path('search/', include('studybuddy.urls'))
     
 ]
