5c62830bef3d579e3f31237e5b673a2b69702b45
John Link <john.link33@gmail.com>
2022-10-24 02:36:35 -0400

    no longer floods the database

--- studybuddy/views.py
+++ studybuddy/views.py
@@ -22,22 +22,33 @@ def logout(request):
 def updateClasses():
     class_mnemonic = requests.get('http://luthers-list.herokuapp.com/api/deptlist?format=json').json()
     #print(class_mnumonic)
+    last_dm = ''
+    last_cn = ''
     for cm in class_mnemonic:
         data = requests.get('http://luthers-list.herokuapp.com/api/dept/' + cm['subject'] + '?format=json').json()
         for each in data:
-            model = LutherClass()
-            model.DeptNnemonic = each['subject']
-            model.CourseNumber = each['course_number']
-            model.CatalogNumber = each['catalog_number']
-            model.SectionNumber = each['course_section']
-            model.ClassName = each['description']
-            model.ProfessorName = each['instructor']['name']
-            model.ProfessorEmail = each['instructor']['email']
-            model.AvailableSeats = each['class_capacity']
-            #model.DaysOfTheWeek = each['meetings'][0]['days']
-            model.Semester = each['semester_code']
-            #print(model)
-            model.save()
+            if not(last_dm == each['subject'] and last_cn == each['catalog_number']):
+                last_dm = each['subject']
+                last_cn = each['catalog_number']
+                try:
+                    if LutherClass.objects.filter(CourseNumber = each['course_number']):
+                        model = LutherClass.objects.filter(CourseNumber = each['course_number'])[0]
+                    else:
+                        model = LutherClass()
+                    model.DeptNnemonic = each['subject']
+                    model.CourseNumber = each['course_number']
+                    model.CatalogNumber = each['catalog_number']
+                    model.SectionNumber = each['course_section']
+                    model.ClassName = each['description']
+                    model.ProfessorName = each['instructor']['name']
+                    model.ProfessorEmail = each['instructor']['email']
+                    model.AvailableSeats = each['class_capacity']
+                    #model.DaysOfTheWeek = each['meetings'][0]['days']
+                    model.Semester = each['semester_code']
+                    #print(model)
+                    model.save()
+                except:
+                    pass
 class ListOfAllClasses(generic.ListView):
     #updateClasses()
     model = LutherClass
