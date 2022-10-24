c0285021a5265df51ec06c589e5925e95274a654
Jim Liang <89550877+uvajim@users.noreply.github.com>
2022-10-24 16:51:37 -0400

    added testing suite

--- studybuddy/tests.py
+++ studybuddy/tests.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 # Create your tests here.
 
 class LoginTestCase(TestCase):
-
-
     def login_test(self):
         user = User.objects.create(username='test_acc')
         user.set_password('12345')
@@ -18,4 +16,22 @@ class LoginTestCase(TestCase):
 
         self.assertEqual(200, response.status_code, response.content)
         self.assertContains(response, 'This profile is already connected to another user account')
+
+class URLTestCases(TestCase):
+    #tests to see that all urls will not return an 404 error.
+
+    def login_test(self):
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def google_login_test(self):
+        response = self.client.get('/accounts/google/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def search_test(self):
+        response = self.client.get('/search')
+        self.assertEqual(response.status_code, 200)
+
+
+
 
