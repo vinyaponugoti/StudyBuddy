f7943357f48e9270f55b2450f6cf20a8c5552024
Sofia Alvarez <sna4wu@virginia.edu>
2022-10-24 14:12:09 -0400

    fixed logo

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / "static"  #os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
