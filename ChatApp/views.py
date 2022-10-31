92b088ebf648e41b9544514b12a4a2e7709da4e0
John Link <john.link33@gmail.com>
2022-10-31 13:48:05 -0400

    chat feature can now login

--- ChatApp/views.py
+++ ChatApp/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render, redirect
  
 def chatPage(request, *args, **kwargs):
     if not request.user.is_authenticated:
-        return redirect("login-user")
+        return redirect("login")
     context = {}
     return render(request, "chat/ChatPage.html", context)
