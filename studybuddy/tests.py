9d3b91c3d41bc1995787f96fc8243321f2986aa4
Jim Liang <89550877+uvajim@users.noreply.github.com>
2022-11-07 17:39:13 -0500

    fixes tests

--- studybuddy/tests.py
+++ studybuddy/tests.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your tests here.
 
 class LoginTestCase(TestCase):
-    def login_test(self):
+    def test_login(self):
         user = User.objects.create(username='test_acc')
         user.set_password('12345')
         user.is_active = True
@@ -20,15 +20,15 @@ class LoginTestCase(TestCase):
 class URLTestCases(TestCase):
     #tests to see that all urls will not return an 404 error.
 
-    def login_test(self):
+    def test_login(self):
         response = self.client.get('/accounts/login/')
         self.assertEqual(response.status_code, 200)
 
-    def google_login_test(self):
+    def test_google_website(self):
         response = self.client.get('/accounts/google/login/')
         self.assertEqual(response.status_code, 200)
 
-    def search_test(self):
+    def test_search(self):
         response = self.client.get('/search')
         self.assertEqual(response.status_code, 200)
 
